457faf484116eb4581674194fd5c85c4add63f99
Sushanata Gupta <sushantagupta007.gmail.com>
2021-08-04 23:41:16 +0600

    Farenhite to Celcius added

--- tem.js
+++ tem.js
@@ -7,3 +7,13 @@ function celciustoFarenhite(celcius){
 
 let Farenhite= celciustoFarenhite(1); 
 console.log("Farehite temp is", Farenhite);  
+
+//Farenhite to celcius
+function FarenhitetoCelcius(Farenhite){
+    let celcius;
+    celcius = (Farenhite-32)*(5/9);
+    return celcius;
+}
+
+let Celcius= FarenhitetoCelcius(33.8); 
+console.log("Celcius temp is", Celcius);  
